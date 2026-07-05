93a05e315580676e5245ce450e881d0c8ef5fac8
VinothaEswaran <vinothaeswarann@gmail.com>
2026-07-05 11:06:24 +0530

    Use NEXT_PUBLIC_API_URL env var for API base URL

--- frontend/lib/api.ts
+++ frontend/lib/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const api = axios.create({ baseURL: "http://localhost:8000/api" });
+const api = axios.create({ baseURL: `${process.env.NEXT_PUBLIC_API_URL}/api` });
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("fd_token");
